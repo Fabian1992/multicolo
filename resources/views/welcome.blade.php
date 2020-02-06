3a3ed87608f958b38475d15a5ff990dfd7b59131
Fabian1992 <fabi.lopez1992@gmail.com>
2020-02-06 17:03:06 -0500

    inicio correcion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
         </style>
     </head>
     <body>
-        fabian lopez
+        fabian lop
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
